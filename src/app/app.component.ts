ca6db8e5fadfcf7dccb148470b6b40b448e77e06
Imelda Zadeja <i.zadeja@albansi.com>
2021-06-30 01:26:06 +0200

    gamification project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'gamification';
+  storedPosts = [];
+  onPostAdded(post) {
+    this.storedPosts.push(post);
+  }
 }
