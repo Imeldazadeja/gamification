60369974e990adb473196cf991193401cc52f085
Imelda Zadeja <i.zadeja@albansi.com>
2021-07-27 00:24:24 +0200

    Add mat-chips autocomplete for lecturer

--- src/app/courses/course/course.component.ts
+++ src/app/courses/course/course.component.ts
@@ -1,8 +1,8 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {COMMA, ENTER} from "@angular/cdk/keycodes";
 import { Observable} from "rxjs";
 import {AuthService} from "../../auth/auth.service";
-import {FormControl} from "@angular/forms";
+import {FormControl, NgForm} from "@angular/forms";
 import {MatAutocomplete, MatAutocompleteSelectedEvent} from "@angular/material/autocomplete";
 import {map, startWith} from "rxjs/operators";
 
@@ -13,27 +13,37 @@ import {map, startWith} from "rxjs/operators";
 })
 export class CourseComponent implements OnInit {
   visible = true;
-  courseCycle: string;
-  selectable = true;
-  removable = true;
-  addOnBlur = true;
-  separatorKeysCodes: number[] = [ENTER, COMMA];
   studyCycleOptions: string [] = ['Bachelor', 'Master', 'PhD'];
+  courseCycle: string;
+
+  /*** chips autocomplete for students ***/
   studentCtrl = new FormControl();
   filteredStudents: Observable<string[]>;
   students: string[] = [];
   allStudents: string[] = [];
+  selectableStudent = true;
+  removableStudent = true;
+  separatorKeysCodesStudent: number[] = [ENTER, COMMA];
+  addOnBlurStudent = true;
+
+
+  /*** chips autocomplete for lecturer ***/
+  lecturerCtrl = new FormControl();
+  filteredLecturer: Observable<string[]>;
+  lecturers: string[] = [];
+  allLecturer: string[] = [];
+  selectableLecturer = true;
+  removableLecturer = true;
+  separatorKeysCodesLecturer: number[] = [ENTER, COMMA];
+  addOnBlurLecturer = true;
+
 
   @ViewChild('studentInput') studentInput: ElementRef<HTMLInputElement>;
+  @ViewChild('lecturerInput') lecturerInput: ElementRef<HTMLInputElement>;
   @ViewChild('auto') matAutocomplete: MatAutocomplete;
 
 
-  constructor(private authService: AuthService) {
-    this.filteredStudents = this.studentCtrl.valueChanges.pipe(
-      startWith(null),
-      map((student: string | null) => student ? this._filter(student) :
-        this.allStudents.slice()));
-  }
+  constructor(private authService: AuthService) {}
 
 
   ngOnInit(): void {
@@ -43,9 +53,24 @@ export class CourseComponent implements OnInit {
       this.studentCtrl.setValue(null);
     });
 
+    this.authService.getLecturer().then(
+      (value: any[]) => {
+        this.allLecturer = value.map(lecturer => lecturer.email);
+        this.lecturerCtrl.setValue(null);
+      });
+
+    this.filteredStudents = this.studentCtrl.valueChanges.pipe(
+      startWith(null),
+      map((student: string | null) => student ? this._filterStudent(student) :
+        this.allStudents.slice()));
+
+    this.filteredLecturer = this.lecturerCtrl.valueChanges.pipe(
+      startWith(null),
+      map((lecturer: string | null) => lecturer ? this._filterLecturer(lecturer) :
+        this.allLecturer.slice()));
   }
 
-  remove(student: string): void {
+  removeStudent(student: string): void {
     const index = this.students.indexOf(student);
 
     if(index >= 0) {
@@ -53,17 +78,49 @@ export class CourseComponent implements OnInit {
     }
   }
 
-  selected(event: MatAutocompleteSelectedEvent): void {
+  removeLecturer(lecturer: string): void {
+    const index = this.lecturers.indexOf(lecturer);
+
+    if(index >= 0) {
+      this.lecturers.splice(index, 1);
+    }
+  }
+
+  selectedStudent(event: MatAutocompleteSelectedEvent): void {
     this.students.push(event.option.viewValue);
     this.studentInput.nativeElement.value = '';
     this.studentCtrl.setValue(null);
   }
 
-  private _filter(value: string): string[] {
+  selectedLecturer(event: MatAutocompleteSelectedEvent): void {
+    this.lecturers.push(event.option.viewValue);
+    this.lecturerInput.nativeElement.value = '';
+    this.lecturerCtrl.setValue(null);
+  }
+
+  private _filterStudent(value: string): string[] {
     const filterValue = value.toLowerCase();
 
     return this.allStudents
       .filter(student =>
       student.toLowerCase().includes(filterValue));
   }
+
+  private _filterLecturer(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.allLecturer
+      .filter(lecturer =>
+        lecturer.toLowerCase().includes(filterValue));
+  }
+  //
+  // onCreateCourse(form: NgForm) {
+  //   if(form.invalid){
+  //     return;
+  //   }
+  //   console.log(form);
+  //   // this.courseService.create().then();
+  //
+  // }
+
 }
