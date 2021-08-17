36879c0dc01d8816d3d13d44031115e6de23da25
Imelda Zadeja <i.zadeja@albansi.com>
2021-08-18 00:46:26 +0200

    delete comment code

--- src/app/auth/auth-data.model.ts
+++ src/app/auth/auth-data.model.ts
@@ -1,30 +1,3 @@
-// export interface AuthData {
-//   firstName: string;
-//   lastName: string;
-//   email: string;
-//   password: string;
-// }
-//
-// export interface AuthDataStudent {
-//   _id: string;
-//   firstName: string;
-//   lastName: string;
-//   email: string;
-//   password: string;
-//   faculty: string;
-//   studyProgramme: string;
-//   studyCycle: string;
-//   registrationDate: Date;
-// }
-//
-// export interface AuthDataLecturer {
-//   _id: string;
-//   firstName: string;
-//   lastName: string;
-//   email: string;
-//   password: string;
-//   faculty: string;
-// }
 export enum UserType {
   admin = 'A',
   student = 'S',
